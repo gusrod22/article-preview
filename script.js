6ea7895396edc283905ce7520a7b11b637a1ae35
rodjus19@gmail.com <gusrod22>
2023-04-08 18:32:32 -0400

    optimized script for responsiveness

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
-const shareButton = document.getElementById('share-button')
-const bottomCard = document.getElementById('bottom-card')
-const userSection = document.querySelector('.user-section')
-const shareInfo = document.querySelector('.share-info')
+const shareButton = document.getElementById("share-button");
+const bottomCard = document.getElementById("bottom-card");
+const userSection = document.querySelector(".user-section");
+const shareInfo = document.querySelector(".share-info");
 
 const originalBackgroundColor = bottomCard.style.backgroundColor;
 
-shareInfo.classList.add('hidden')
+shareInfo.classList.add("hidden");
 
-shareButton.addEventListener('click', () => {
-    userSection.classList.toggle('hidden');
-    shareInfo.classList.toggle('hidden');
-    
-    if (userSection.classList.contains('hidden')) {
-      // Both userSection and shareInfo are hidden, so change the background color to the darker value
-      bottomCard.style.backgroundColor = 'hsl(217, 19%, 35%)';
-      shareButton.style.backgroundColor = 'hsl(214, 17%, 51%)';
-      document.querySelector('.svg-container svg path').setAttribute('fill', '#fff');
+shareButton.addEventListener("click", () => {
+  if (window.innerWidth >= 1440) {
+    // viewport width is 1440px or greater, so only change the design for shareButton and shareInfo
+    shareInfo.classList.toggle("hidden");
+    shareButton.classList.toggle("active");
 
+    if (shareButton.classList.contains("active")) {
+      shareButton.style.backgroundColor = "hsl(214, 17%, 51%)";
+      document
+        .querySelector(".svg-container svg path")
+        .setAttribute("fill", "#fff");
+      shareInfo.style.backgroundColor = "hsl(217, 19%, 35%)"
     } else {
-      // Either userSection or shareInfo is not hidden, so change the background color back to its original value
-      bottomCard.style.backgroundColor = originalBackgroundColor;
-      shareButton.style.backgroundColor = 'hsl(210, 46%, 95%)';
-      document.querySelector('.svg-container svg path').setAttribute('fill', '#6E8098');
-
-
+      shareButton.style.backgroundColor = "hsl(210, 46%, 95%)";
+      document
+        .querySelector(".svg-container svg path")
+        .setAttribute("fill", "#6E8098");
     }
-  });
+  } else {
+    // viewport width is less than 1440px, so change the design for both userSection and shareInfo
+    userSection.classList.toggle("hidden");
+    shareInfo.classList.toggle("hidden");
+
+    if (userSection.classList.contains("hidden")) {
+      bottomCard.style.backgroundColor = "hsl(217, 19%, 35%)";
+    } else {
+      bottomCard.style.backgroundColor = originalBackgroundColor;
+    }
+  }
+});
